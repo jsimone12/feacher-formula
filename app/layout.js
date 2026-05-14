f45bd549a641e1b3eeb2e93cf80f5747dada894b
Jasmine Summers <info@jsimonesolutions.com>
2026-05-14 10:28:51 -0400

    Create layout.js for CAT Adjuster Quiz app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "CAT Adjuster Quiz",
+  description: "Find out if this is the side hustle for you",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
